99ff8ba8a9114dd6cf8b16cf1e618abc2f530f3c
rgsinson05 <rgestallosinson@gmail.com>
2026-09-09 00:31:34 +0800

    Create MySQL database connection

---
+++ includes/database.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$dsn = 'mysql:host=' . $_ENV['DB_HOST']
+     . ';dbname=' . $_ENV['DB_NAME']
+     . ';charset=utf8mb4';
+
+$pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+]);
